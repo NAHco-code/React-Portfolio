66716b6ed6534c392248e49f8b341cc8729ce615
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-23 23:10:58 -0500

    Build: render header component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import React from 'react';
 // import { Switch, Route } from 'react-router-dom';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <Nav />
+      <Header />
     </div>
   );
 }
