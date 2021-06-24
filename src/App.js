1b0d693115810a256656856264bd8b1dea442103
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-24 00:45:23 -0500

    Docs: change component file structure

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Contact />
     </div>
   );
 }
---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+
+export default function Contact () {
+    return (
+        <div>
+            <Footer />
+            <img alt='' src='../images/cloud-fade-white.JPG'/>
+        </div>
+    );
+}
