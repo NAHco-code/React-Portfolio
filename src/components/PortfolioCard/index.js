20957ce07eb16fb1cee167304fcee01a9904c3d2
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-25 03:13:29 -0500

    Detail portfolio page

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './index.css';
 
 export default function PortfolioCard ({gitURL, liveURL, title, id}) {
     return (
 
         <div className="column long-disp">
-            <a href={gitURL} target='_blank' rel='noreferrer'><div title=' View Code ' className="thumbnail first-feat" id={id+'1'}><div className='port-screen'></div></div></a>
+            <a href={gitURL} target='_blank' rel='noreferrer'>
+                <div title=' View Code ' className="thumbnail first-feat" id={id + '1'}>
+                    <div className='port-screen'></div>
+                </div>
+            </a>
             <h5>{title}</h5>
-            <a href={liveURL} target='_blank' rel='noreferrer'><div title=' View Live ' className="thumbnail sec-feat" id={id+'2'}><div className='port-screen2'></div></div></a>
+            <a href={liveURL} target='_blank' rel='noreferrer'>
+                <div title=' View Live ' className="thumbnail sec-feat" id={id + '2'}>
+                    <div className='port-screen2'></div>
+                </div>
+            </a>
         </div>
+
     );
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import PortfolioCard from '../components/PortfolioCard';
 import projects from '../data/projects.json';
+import fade3 from '../images/black-white-fade-WHITE.PNG';
 
 export default function Portfolio () {
     return (
-        <div className='black-background'>
-            <h1>Portfolio.</h1>
-            <div className='portfolio-header'>Some ground to stand on.</div>
+        <div>
+            <div class="portfolio-page padding" id='portfolio-page'>
+                <div class="row">
+                    <h2>portfolio.</h2>
+                    <p>I enjoy an innovative and
+                    judicious approach when solving problems. Developed design skills enable
+                    craft and optimization of elegant, user-focused websites
+                    and applications.
+                    </p>
+                    <p>Backgrounds in fine art, social work and
+                    hospitality industries, provide in-depth knowledge
+                    of dynamic team collaboration, demanding clients,
+                    sensitive information & business objectives, as well as
+                    sales, marketing and design.
+                    </p>
+                </div>
 
-            <div className="row small-up-2 medium-up-3 large-up-4 portfolio-sec">
-                {projects.map( project => ( <PortfolioCard {...project}/>)) }
+                <div className="row small-up-2 medium-up-3 large-up-4 portfolio-sec">
+                    {projects.map( project => ( <PortfolioCard {...project}/>)) }
+                </div>
             </div>
+            <img alt='' className='fade3' src={fade3}/>
         </div>
     );
 }
