fa91b2903d811102defbcce2b86fa3e800ae8525
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-25 13:46:28 -0500

    Hash Router working

--- src/App.js
+++ src/App.js
@@ -1,53 +1,37 @@
 import './App.css';
 import './background-img.css';
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './components/Nav';
 import Home from './pages/Home';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-
-    <Router
-      basename='/'
-    >
-      <div className='App'>
+    <div className='App'>
       <Nav />
-      <Home />
-      <About />
-      <Portfolio />
-      <Contact />
-      </div>
-    </Router>
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/about">
+              <About />
+            </Route>
+            <Route exact path="/portfolio">
+              <Portfolio />
+            </Route>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+          </Switch>
+        </Router>
+      <Footer />
+    </div>
   );
 }
 
 export default App;
-
-
-{/*
-
-  <Router
-  basename='/'
-  >
-
-  <Switch>
-    <Route path='/' component={Home} />
-    <Route path='/about' component={About} />
-    <Route path='/portfolio' component={Portfolio} />
-    <Route path='/contact' component={Contact} />
-  </Switch>
-
-</Router>*/}
-
-
-
-
-// <div className='header-img' alt=''></div>
-// <div className='header-img-mirror' alt=''></div>;
-//<div className='fade-to-about' alt=''></div>
-//<div className='fade-to-portfolio' alt=''></div>
-//<div className='fade-to-contact' alt=''></div>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import homeIcon from '../../images/right-arrow-round.png';
 import instaIcon from '../../images/insta-white.PNG';
 import githubIcon from '../../images/github-white-blk-outline.PNG';
@@ -12,15 +12,15 @@ export default function Nav () {
         <div className="sticky">
             <div className="title-bar nav">
                 <div className="title-bar-left nav-sec nav-1">
-                    <Link to='/home'>
+                    <a href='/react-portfolio/#/home'>
                         <img alt='' src={homeIcon} className='home-icon'/>
-                    </Link>
+                    </a>
                 </div>
                 <div className="title-bar-center nav-sec nav-2">
-                    <a href='#about-page' className="nav-links">about</a>
-                    <a href='#portfolio-page' className="nav-links">portfolio</a>
+                    <a href='/react-portfolio/#/about' className="nav-links">about</a>
+                    <a href='/react-portfolio/#/portfolio' className="nav-links">portfolio</a>
                     <a href='../resume/kw-resume-pdf.pdf' download className="nav-links">resume</a>
-                    <a href='#contact-page' className="nav-links">contact</a>
+                    <a href='/react-portfolio/#/contact' className="nav-links">contact</a>
                 </div>
                 <div className="title-bar-right nav-sec nav-3">
 
