ec127d482560c8aa1059a11e12f7d4fb1e80bc9c
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-24 20:53:58 -0500

    Render components on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import PreviewCard from '../components/PreviewCard';
+
 export default function Home () {
     return (
         <div>
             <Header />
-            <div className=''>SOME OF MY LATEST WORK</div>
-            <div>
+            <div className='black-background'>SOME OF MY LATEST WORK</div>
+            <div className='black-background'>
                 <ul>
                     <PreviewCard />
                     <PreviewCard />
