37139c858d1083dab96a937a05241aa0798a7e73
Kathryn Wilkinson <kathrynxwilk@gmail.com>
2021-06-24 22:18:08 -0500

    Render page footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,24 @@ import './index.css';
 
 export default function Footer () {
     return (
-        <div>
-            <h2 className='footer'>Footer</h2>
-        </div>
+        <footer>
+            <div class="footer-wrapper-1">
+            <div class="copyright footer-text">
+                &copy; K WILKINSON 2021
+            </div>
+            <div class="term-of-use">
+                <a href='#' class="footer-text">TERMS OF USE</a>
+            </div>
+            <div class="privacy-policy">
+                <a href='#' class="footer-text">PRIVACY POLICY</a>
+            </div>
+            <div class="download-resume">
+                <a href='#' class="footer-text">RESUME</a>
+            </div>
+            <div class="contact">
+                <a href='#' class="footer-text">CONTACT</a>
+            </div>
+            </div>
+        </footer>
     );
 }
